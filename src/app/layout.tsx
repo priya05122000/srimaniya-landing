1d5a736a3e06adc8944c60a513f80322619dcb1a
04-AKILESH <akileshnagarajan123@gmail.com>
2026-02-18 13:01:04 +0530

    Ad Gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,6 +34,22 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="robots" content="noindex, nofollow"></meta>
+          {/* Google Tag Manager */}
+          <Script id="gtm-script" strategy="afterInteractive">
+            {`
+              (function(w,d,s,l,i){
+                w[l]=w[l]||[];
+                w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),
+                    dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;
+                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-TLLR36TQ');
+            `}
+          </Script>
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=AW-17861303334"
           strategy="afterInteractive"
@@ -49,6 +65,15 @@ export default function RootLayout({
         </Script>
       </head>
       <body className="antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TLLR36TQ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <LoadingContext.Provider value={{ loading, setLoading }}>
           <CaptchaWrapper>
             {/* Loader */}
