e04f2a340c3e98b0a246eea8859b8defe7331bf7
04-AKILESH <akileshnagarajan123@gmail.com>
2026-03-11 18:29:20 +0530

    Pixel tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,9 +35,9 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="robots" content="noindex, nofollow"></meta>
-          {/* Google Tag Manager */}
-          <Script id="gtm-script" strategy="afterInteractive">
-            {`
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
               (function(w,d,s,l,i){
                 w[l]=w[l]||[];
                 w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
@@ -49,7 +49,7 @@ export default function RootLayout({
                 f.parentNode.insertBefore(j,f);
               })(window,document,'script','dataLayer','GTM-TLLR36TQ');
             `}
-          </Script>
+        </Script>
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=AW-17861303334"
           strategy="afterInteractive"
@@ -63,6 +63,30 @@ export default function RootLayout({
               gtag('config', 'AW-17861303334');
             `}
         </Script>
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}
+            (window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
+
+            fbq('init', '1590176728705087');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1590176728705087&ev=PageView&noscript=1"
+          />
+        </noscript>
       </head>
       <body className="antialiased">
         {/* Google Tag Manager (noscript) */}
